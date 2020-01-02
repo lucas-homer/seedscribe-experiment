816a993d63b07c54913484cb67c395200c0c84dc
Lucas Homer <lucashomer@lucass-mbp.home>
2020-01-01 17:49:27 -0800

    gatsby plugin netlify identity widget

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,6 +15,7 @@ module.exports = {
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
+    `gatsby-plugin-netlify-identity-widget`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
